2ee406dca2feab75acd1b5e9b5da7762ab96d64d
Anthony <acnuzzi@student.ysu.edu>
2017-11-14 21:45:01 -0500

    navbar images Added

---
+++ resource/js/main.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function () {
+  $(".carousel").slick({
+   arrows: false,
+   autoplay: true,
+   autoplaySpeed: 4000,
+   fade: true,
+   dots: true
+ });
+});
+
+/*
+$(document).ready(function() {
+ if ($(".carousel").is("jumbotron-bike")) {
+   $(".jumbotron-image-navbar").show({display: none});
+ }
+ });
+*/
+
+/* $(document).ready(function(){
+    if( $(".carousel").is(".jumbotron-bike")){
+         $(".jumbotron-image-navbar").css("display", "none");
+     });
+ });
+*/
