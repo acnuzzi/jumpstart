7621a8893d0e7894d8701753343af897b72c8287
Anthony <acnuzzi@student.ysu.edu>
2017-11-18 02:12:27 -0500

    jumbotron image Added

--- resource/js/main.js
+++ resource/js/main.js
@@ -13,16 +13,31 @@ $(document).ready(function () {
 
 
 
- var lastScrollTop = 0;
- $(window).scroll(function(){
-   var scrollTop = $(this).scrollTop();
-   if (scrollTop - lastScrollTop > 50){
-     var navHeight = $('.navbar').css('height');
-     $('.navbar').animate({top: '-' + navHeight}, 150);
-     lastScrollTop = scrollTop;
-   } else if (lastScrollTop - scrollTop > 50) {
-     $('.navbar').animate({top: '0px'}, 150);
-     lastScrollTop = scrollTop;
-   }
+var distanceScroll = 0;
+$(window).scroll(function() {
+  var distanceStores = $(this).distanceStores();
+  if (distanceStores - distanceScroll > 50){
+    var navHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + naveHeight}, 150);
+    distanceScroll = distanceStores;
+  } else if (distanceScroll - distanceStores > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+    distanceScroll = distanceStores;
+  }
  });
 });
+
+/*
+var lastScrollTop = 0;
+$(window).scroll(function(){
+  var scrollTop = $(this).scrollTop();
+  if (scrollTop - lastScrollTop > 50){
+    var navHeight = $('.navbar').css('height');
+    $('.navbar').animate({top: '-' + navHeight}, 150);
+    lastScrollTop = scrollTop;
+  } else if (lastScrollTop - scrollTop > 50) {
+    $('.navbar').animate({top: '0px'}, 150);
+    lastScrollTop = scrollTop;
+  }
+});
+*/
